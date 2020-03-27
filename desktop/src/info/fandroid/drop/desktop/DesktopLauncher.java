a8328fc665ba5f95e6e1abc8c3a8025b19b88fde
katya <katya@example.ru>
2020-03-27 22:04:50 +0300

    version 1, drops, only drops

---
+++ desktop/src/info/fandroid/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package info.fandroid.drop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import info.fandroid.drop.Drop;
+import info.fandroid.drop.GameScreen;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "GameScreen";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new Drop(), config);
+	}
+}
